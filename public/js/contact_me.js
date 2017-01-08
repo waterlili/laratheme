d0f01cb1b0c5706732e871d4f0de76a5d3bbc6c4
niloufar <nw.tahmasebi@gmail.com>
2017-01-08 22:45:28 +0330

    validate contact form

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -23,52 +23,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            // $.ajax({
-            //     // url: "././mail/contact_me.php",
-            //     url: "/contactus",
-            //     type: "POST",
-            //     data: {
-            //         name: name,
-            //         phone: phone,
-            //         email: email,
-            //         message: message,
-            //     },
-            //     // data: {_token: CSRF_TOKEN},
-            //     dataType: 'json',
-            //     cache: false,
-            //     success: function(msg) {
-
-            //          alert(msg.arr);
-            //         // Success message
-            //         if ( msg.status === 'success' ) {
-
-            //         $('#success').html("<div class='alert alert-success'>");
-            //         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            //             .append("</button>");
-            //         $('#success > .alert-success')
-            //             .append("<strong>Your message has been sent. </strong>");
-            //         $('#success > .alert-success')
-            //             .append('</div>').delay(3000).fadeOut("slow");
-
-            //         //clear all fields
-            //         $('#contactForm').trigger("reset");
-
-            //       }
-            //     },
-            //     error: function(msg) {
-            //         // Fail message
-            //         if ( msg.status === 'failed' ) {
-            //         $('#success').html("<div class='alert alert-danger'>");
-            //         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            //             .append("</button>");
-            //         $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-            //         $('#success > .alert-danger').append('</div>').delay(3000).fadeOut("slow");
-            //         //clear all fields
-            //         $('#contactForm').trigger("reset");
-            //        }
-
-            //     },
-            // });
+            
             var form = $("#contactForm");
 
             $.ajax({
